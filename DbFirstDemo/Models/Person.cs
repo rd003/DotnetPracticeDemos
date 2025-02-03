eb82fef7d3eb5f78110cd65bc8af3391a413ffc2
Ravindra Devrani <ravindra.devrani003@gmail.com>
2025-02-03 19:03:53 +0530

    DbFirstDemo

---
+++ DbFirstDemo/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DbFirstDemo.Models;
+
+public partial class Person
+{
+    public int PersonId { get; set; }
+
+    public string PersonName { get; set; } = null!;
+
+    public string PersonEmail { get; set; } = null!;
+}
