3679b9fc93a82ff6ca22247cae6165f93beb9d69
Ravindra Devrani <ravindra.devrani003@gmail.com>
2025-09-04 10:54:11 +0530

    feat: saved

---
+++ AzureServiceBusDemo/AzureServiceBus.Reciever/Program.cs
@@ -0,0 +1,69 @@
+﻿// reciever
+using Azure.Messaging.ServiceBus;
+using Azure.Identity;
+using System.Text.Json;
+
+// the client that owns the connection and can be used to create senders and receivers
+ServiceBusClient client;
+
+// the processor that reads and processes messages from the queue
+ServiceBusProcessor processor;
+
+var clientOptions = new ServiceBusClientOptions()
+{
+    TransportType = ServiceBusTransportType.AmqpWebSockets
+};
+client = new ServiceBusClient("rd003servicebus.servicebus.windows.net",
+    new DefaultAzureCredential(), clientOptions);
+
+// create a processor that we can use to process the messages
+processor = client.CreateProcessor("MyQueue", new ServiceBusProcessorOptions());
+
+try
+{
+    // add handler to process messages
+    processor.ProcessMessageAsync += MessageHandler;
+
+    // add handler to process any errors
+    processor.ProcessErrorAsync += ErrorHandler;
+
+    // start processing 
+    await processor.StartProcessingAsync();
+
+    Console.WriteLine("Wait for a minute and then press any key to end the processing");
+    Console.ReadKey();
+
+    // stop processing 
+    Console.WriteLine("\nStopping the receiver...");
+    await processor.StopProcessingAsync();
+    Console.WriteLine("Stopped receiving messages");
+}
+finally
+{
+    await processor.DisposeAsync();
+    await client.DisposeAsync();
+}
+
+// handle received messages
+async Task MessageHandler(ProcessMessageEventArgs args)
+{
+    string body = args.Message.Body.ToString();
+
+    Console.WriteLine($"Body: {body}");
+
+    var personData = JsonSerializer.Deserialize<Person>(body);
+
+    Console.WriteLine($"Received: {personData.ToString()}");
+
+    // complete the message. message is deleted from the queue. 
+    await args.CompleteMessageAsync(args.Message);
+}
+
+// handle any errors when receiving messages
+Task ErrorHandler(ProcessErrorEventArgs args)
+{
+    Console.WriteLine(args.Exception.ToString());
+    return Task.CompletedTask;
+}
+
+record Person(string FirstName, string LastName);
