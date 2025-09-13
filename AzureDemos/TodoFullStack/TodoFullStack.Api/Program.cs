b9e13537068331eb0b00cd65277dcd2ceb852aae
Ravindra Devrani <ravindra.devrani003@gmail.com>
2025-09-13 23:05:30 +0530

    feat: saved

--- AzureDemos/TodoFullStack/TodoFullStack.Api/Program.cs
+++ AzureDemos/TodoFullStack/TodoFullStack.Api/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using TodoFullStack.Api.Models;
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,7 +10,19 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-builder.Services.AddDbContext<AppDbContext>(op=>op.UseInMemoryDatabase("InMem"));
+builder.Services.AddDbContext<AppDbContext>(op => op.UseInMemoryDatabase("InMem"));
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:4200",
+                                              "http://www.contoso.com");
+                          policy.AllowAnyMethod();
+                          policy.AllowAnyHeader();
+                      });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -18,9 +32,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
-
 app.MapControllers();
 
 using var scope = app.Services.CreateScope();
---
+++ AzureDemos/TodoFullStack/TodoFullStack.Client/src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    API_BASE_URL: "http://localhost:5221",
+    production: false
+};
